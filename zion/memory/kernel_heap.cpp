4380590af239a44fbde17f3df9afbdcb73e0000e
Drew Galbraith <dgalbraith33@gmail.com>
2023-05-18 11:29:44 -0700

    Add new and delete operator implementations to the kernel heap.
    For now delete does nothing.

--- zion/memory/kernel_heap.cpp
+++ zion/memory/kernel_heap.cpp
@@ -3,8 +3,22 @@
 #include "debug/debug.h"
 #include "memory/paging_util.h"
 
+namespace {
+
+static KernelHeap* gKernelHeap = nullptr;
+
+KernelHeap& GetKernelHeap() {
+  if (!gKernelHeap) {
+    panic("Kernel Heap not initialized.");
+  }
+  return *gKernelHeap;
+}
+}  // namespace
+
 KernelHeap::KernelHeap(uint64_t lower_bound, uint64_t upper_bound)
-    : next_addr_(lower_bound), upper_bound_(upper_bound) {}
+    : next_addr_(lower_bound), upper_bound_(upper_bound) {
+  gKernelHeap = this;
+}
 
 void* KernelHeap::Allocate(uint64_t size) {
   if (next_addr_ + size >= upper_bound_) {
@@ -15,3 +29,7 @@ void* KernelHeap::Allocate(uint64_t size) {
   next_addr_ += size;
   return reinterpret_cast<void*>(address);
 }
+
+void* operator new(uint64_t size) { return GetKernelHeap().Allocate(size); }
+
+void operator delete(void*, uint64_t) {}
