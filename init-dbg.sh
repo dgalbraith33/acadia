3243b6024b99c1c02ff5cef8e3cea6e07075c763
Drew Galbraith <dgalbraith33@gmail.com>
2023-06-07 09:29:38 -0700

    Update init bg script to inclue AR

--- init-dbg.sh
+++ init-dbg.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-cmake -B builddbg/ -G Ninja -D CMAKE_CXX_COMPILER=x86_64-elf-gcc -D CMAKE_ASM-ATT_COMPILER=x86_64-elf-gcc -D CMAKE_BUILD_TYPE=Debug 
+cmake -B builddbg/ -G Ninja -D CMAKE_CXX_COMPILER=x86_64-elf-gcc -D CMAKE_ASM-ATT_COMPILER=x86_64-elf-gcc -D CMAKE_AR=`which x86_64-elf-ar` -D CMAKE_BUILD_TYPE=Debug 
