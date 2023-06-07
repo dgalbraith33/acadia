a8a4f8d9abe12d1737de18278e0e7602a7208e2d
Drew Galbraith <dgalbraith33@gmail.com>
2023-06-07 00:25:04 -0700

    Add checks to mammoth thread lib

--- lib/mammoth/src/thread.cpp
+++ lib/mammoth/src/thread.cpp
@@ -1,6 +1,8 @@
-#include "include/mammoth/thread.h"
+#include "mammoth/thread.h"
 
 #include <zcall.h>
+
+#include "mammoth/debug.h"
 
 namespace {
 
@@ -13,7 +15,8 @@ extern "C" void thread_entry(Thread::Entry entry, void* arg1) {
 }  // namespace
 
 Thread::Thread(Entry e, const void* arg1) {
-  ZThreadCreate(Z_INIT_PROC_SELF, &thread_cap_);
-  ZThreadStart(thread_cap_, reinterpret_cast<uint64_t>(thread_entry),
-               reinterpret_cast<uint64_t>(e), reinterpret_cast<uint64_t>(arg1));
+  check(ZThreadCreate(Z_INIT_PROC_SELF, &thread_cap_));
+  check(ZThreadStart(thread_cap_, reinterpret_cast<uint64_t>(thread_entry),
+                     reinterpret_cast<uint64_t>(e),
+                     reinterpret_cast<uint64_t>(arg1)));
 }
