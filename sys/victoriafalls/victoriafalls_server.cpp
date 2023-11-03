abf09d8fce5837aa88e7a900baafe8e8070cd0d5
Drew Galbraith <dgalbraith33@gmail.com>
2023-11-02 20:24:33 -0700

    [VictoriaFalls] Split the path when opening a file.

--- sys/victoriafalls/victoriafalls_server.cpp
+++ sys/victoriafalls/victoriafalls_server.cpp
@@ -1,5 +1,7 @@
 #include "victoriafalls_server.h"
 
+#include <glacier/string/str_split.h>
+#include <mammoth/debug.h>
 #include <zcall.h>
 
 glcr::ErrorOr<glcr::UniquePtr<VFSServer>> VFSServer::Create() {
@@ -8,7 +10,16 @@ glcr::ErrorOr<glcr::UniquePtr<VFSServer>> VFSServer::Create() {
   return glcr::UniquePtr<VFSServer>(new VFSServer(endpoint_cap));
 }
 
-glcr::ErrorCode VFSServer::HandleOpenFile(const OpenFileRequest&,
+glcr::ErrorCode VFSServer::HandleOpenFile(const OpenFileRequest& request,
                                           OpenFileResponse&) {
+  auto path_tokens = glcr::StrSplit(request.path(), '/');
+  for (uint64_t i = 0; i < path_tokens.size(); i++) {
+    dbgln("Token %u: '%s'", i, glcr::String(path_tokens.at(i)).cstr());
+  }
+  // Require all paths to be absolute rather than relative.
+  // If the path starts with '/' then the first token will be empty.
+  if (path_tokens.at(0) != "") {
+    return glcr::INVALID_ARGUMENT;
+  }
   return glcr::UNIMPLEMENTED;
 }
