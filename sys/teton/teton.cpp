84d3caaba500428ca35dc6c3e2df3055286e0e6a
Drew Galbraith <dgalbraith33@gmail.com>
2023-11-15 16:41:51 -0800

    [Teton] Get framebuffer info from yellowstone.

--- sys/teton/teton.cpp
+++ sys/teton/teton.cpp
@@ -1,11 +1,19 @@
 #include <mammoth/debug.h>
 #include <mammoth/init.h>
+#include <yellowstone/yellowstone.yunq.client.h>
 
 uint64_t main(uint64_t init_port) {
   ParseInitPort(init_port);
 
   dbgln("Teton Starting");
+
   // 1. Set up framebuffer.
+  YellowstoneClient client(gInitEndpointCap);
+
+  FramebufferInfo framebuffer;
+  RET_ERR(client.GetFramebufferInfo({}, framebuffer));
+  dbgln("FB addr {x}, width {} , height {}", framebuffer.address_phys(),
+        framebuffer.width(), framebuffer.height());
 
   // 2. Parse a font file.
 
