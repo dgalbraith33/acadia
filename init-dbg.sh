40ea359e9f818aaba16c8a6df76ec5a8141fe31a
Drew Galbraith <dgalbraith33@gmail.com>
2023-11-16 20:13:31 -0800

    Fix init file to set proper install root.h

--- init-dbg.sh
+++ init-dbg.sh
@@ -4,4 +4,5 @@ CWD="$(pwd)"
 BIN=$CWD/toolchain/local/bin
 GCC=$BIN/x86_64-pc-acadia-gcc
 AR=$BIN/x86_64-pc-acadia-ar
-cmake -B builddbg/ -G Ninja -D CMAKE_CXX_COMPILER=${GCC} -D CMAKE_ASM-ATT_COMPILER=${GCC} -D CMAKE_AR=${AR} -D CMAKE_BUILD_TYPE=Debug 
+SYSROOT=$CWD/sysroot
+cmake -B builddbg/ -G Ninja -D CMAKE_CXX_COMPILER=${GCC} -D CMAKE_ASM-ATT_COMPILER=${GCC} -D CMAKE_AR=${AR} -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=${SYSROOT}
