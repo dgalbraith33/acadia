8bedc80cafee0bee0c93eb7290a9967f1dbbffa4
Drew Galbraith <dgalbraith33@gmail.com>
2023-11-24 15:04:19 -0800

    [Zion] Add a max depth to stack unwinds.

--- zion/common/stack_unwind.cpp
+++ zion/common/stack_unwind.cpp
@@ -9,12 +9,14 @@ bool IsValid(uint64_t* rbp) { return rbp && *rbp != kStackBaseSentinel; }
 }  // namespace
 
 void StackUnwind(uint64_t rbp) {
+  uint64_t depth_limit = 10;
   dbgln("-- Begin Stack --");
   uint64_t* rbp_ptr = reinterpret_cast<uint64_t*>(rbp);
-  while (IsValid(rbp_ptr)) {
+  while (IsValid(rbp_ptr) && depth_limit > 0) {
     uint64_t rip = *(rbp_ptr + 1);
     dbgln("RIP: {x}", rip);
     rbp_ptr = reinterpret_cast<uint64_t*>(*rbp_ptr);
+    depth_limit--;
   }
   dbgln("-- End Stack --");
 }
