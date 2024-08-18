c1db6cb11f62379ee9a61424ea4d4be85528f3ae
Drew Galbraith <dgalbraith33@gmail.com>
2024-08-18 12:45:53 -0700

    [Yellowstone][Mammoth] Delete unused cpp code.

--- lib/mammoth/ipc/endpoint_client.cpp
+++
@@ -1,9 +0,0 @@
-#include "ipc/endpoint_server.h"
-
-namespace mmth {
-
-glcr::UniquePtr<EndpointClient> EndpointClient::AdoptEndpoint(z_cap_t cap) {
-  return glcr::UniquePtr<EndpointClient>(new EndpointClient(cap));
-}
-
-}  // namespace mmth
